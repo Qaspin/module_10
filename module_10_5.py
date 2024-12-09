7d515f3dffe297999260bf63d795245d0f3f9c4e
Александр <1117-zrp@mail.ru>
2024-12-09 15:17:37 +0700

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+from datetime import datetime
+
+def read_info(name):
+    all_data = []
+    with open(name) as file:
+        for line in file:
+            all_data.append(line)
+
+
+
+if __name__ == '__main__':
+    filenames = [f'./file_{number}.txt' for number in range(1, 5)]
+
+    start_time = datetime.now()
+    for name in filenames:
+        read_info(name)
+    print(datetime.now() - start_time, '(линейный)')
+
+    start_time = datetime.now()
+    with Pool(processes=4) as pool:
+        pool.map(read_info, filenames)
+    print(datetime.now() - start_time, '(многопроцессный)')
